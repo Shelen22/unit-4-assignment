477f0fd6c97691a6019c3d25333d3de50432ef68
Shailendra Singh <shailendrauday5@gmail.com>
2021-12-02 20:16:56 +0530

    m

--- MVC/src/models/student.model.js
+++ MVC/src/models/student.model.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const studentSchema = new mongoose.Schema(
     {
-        roll_id: { type: Number, required: true },
+        roll_no: { type: Number, required: true },
         current_batch: { type: String, required: true },
         user_id: {
             type: mongoose.Schema.Types.ObjectId,
